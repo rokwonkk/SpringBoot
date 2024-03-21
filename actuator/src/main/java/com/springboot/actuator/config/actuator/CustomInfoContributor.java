d78398cb71c07013db841ab142b7e5039ecce7e1
로건리 <jirisan8147@gmail.com>
2024-03-21 20:31:02 +0900

    커스텀 액추에이터 커스텀 구현체 생성

---
+++ actuator/src/main/java/com/springboot/actuator/config/actuator/CustomInfoContributor.java
@@ -0,0 +1,19 @@
+package com.springboot.actuator.config.actuator;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Component
+public class CustomInfoContributor implements InfoContributor {
+    @Override
+    public void contribute(Info.Builder builder) {
+        Map<String, Object> content = new HashMap<>();
+        content.put("code-info", "InfoContributor 구현체에서 정의한 정보");
+        builder.withDetail("custom-info-contributor", content);
+    }
+}

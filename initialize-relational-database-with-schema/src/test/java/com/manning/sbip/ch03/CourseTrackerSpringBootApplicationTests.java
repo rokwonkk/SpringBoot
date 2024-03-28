135c9184a89dd8ff096f12a19f9eca401365825c
로건리 <jirisan8147@gmail.com>
2024-03-29 00:32:21 +0900

    h2 db 초기화 테스트

---
+++ initialize-relational-database-with-schema/src/test/java/com/manning/sbip/ch03/CourseTrackerSpringBootApplicationTests.java
@@ -0,0 +1,41 @@
+package com.manning.sbip.ch03;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class CourseTrackerSpringBootApplicationTests {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void whenCountAllCoursesThenExpectFiveCourses() throws SQLException {
+
+        ResultSet rs = null;
+        int noOfCourses = 0;
+
+        try(PreparedStatement ps = dataSource.getConnection().prepareStatement("SELECT  COUNT(1) FROM COURSES"))
+        {
+            rs = ps.executeQuery();
+            while (rs.next()){
+                noOfCourses = rs.getInt(1);
+            }
+            assertThat(noOfCourses).isEqualTo(5L);
+        }
+        finally {
+            if(rs != null){
+                rs.close();
+            }
+        }
+    }
+}
